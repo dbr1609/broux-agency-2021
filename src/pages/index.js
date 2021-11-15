8e86fa36ed21616b4de13d5fdf5a9fb5ac97175d
Dominiek <dominiekbroux@hotmail.com>
2021-11-15 15:59:42 +0100

    ssl error on static image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,18 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
+    <main>
       <Layout pageTitle="Welcome to Broux Agency!">
       <p>Lorem ipsum</p>
+      <StaticImage
+        alt="random artist picture"
+        src="https://omnivrembo.be/images/profidak.jpg"
+      />
       </Layout>
+    </main>
+      
   )
 }
 export default IndexPage
