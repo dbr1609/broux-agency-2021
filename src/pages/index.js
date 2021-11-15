8c04af1ec876fd5253477079bebfea4cc2711a05
Dominiek Broux <dominiekbroux@hotmail.com>
2021-11-15 20:48:22 +0100

    Gatsby Plugins finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,12 @@ const IndexPage = () => {
       <Layout pageTitle="Welcome to Broux Agency!">
       <p>Lorem ipsum</p>
       <StaticImage
-        alt="random artist picture"
-        src="https://omnivrembo.be/images/profidak.jpg"
+        alt="dj mandy"
+        src="https://www.djmandyofficial.be/content/img/3/1124-12672-20200307-215859-reverze-bj3i2815-2048px-912f35f9d96c30ec.jpg"
+      />
+      <StaticImage
+        alt="day-mar"
+        src="../images/day-mar.jpg"
       />
       </Layout>
     </main>
