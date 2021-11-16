9f3f8ee48f9151931dfbc5a463e4a87655d584f0
Dominiek <dominiekbroux@hotmail.com>
2021-11-16 16:06:10 +0100

    undefined error

--- src/pages/artists/index.js
+++ src/pages/artists/index.js
@@ -1,12 +1,16 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import Layout from '../components/layout'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+
 const ArtistsPage = ({data: {allWpArtist: {edges}}}) => {
     return (
         <Layout pageTitle="Artists of Broux Agency">
             {edges.map((item) => {
                 const artist = item.node.artistMeta;
-                return <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+                const slug = item.node.slug;
+                return <Link to={`/artists/${slug}`}>
+                    <p key={item.node.id}>{artist.firstName} {artist.lastName}</p>
+                </Link> 
             })}
         </Layout>
     )
